4debddd71abcd97a7c5d82d6cfc2266b59c5a43b
Christoph Wulf <christoph@wulf.technology>
2026-06-07 15:25:43 +0200

    Quantize

--- src/quantize.py
+++ src/quantize.py
@@ -83,6 +83,7 @@ def process_model(model_id: str, s3_client):
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.bfloat16,
         bnb_4bit_use_double_quant=True,
+        llm_int8_enable_fp32_cpu_offload=True  # Allow offloading at edge of OOM
     )
     
     # 2. Download and load into VRAM
