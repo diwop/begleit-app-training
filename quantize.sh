7fbb0396ca5d1b7c606b8ebc0d0a3e56e97fcf38
Christoph Wulf <christoph@wulf.technology>
2026-06-07 15:37:51 +0200

    Quantize

--- quantize.sh
+++ quantize.sh
@@ -7,7 +7,4 @@ cd /runner/repo/
 uv export --no-emit-project --format requirements-txt > requirements.txt
 uv pip install -r requirements.txt
 
-# dynamically bolt on bitsandbytes lib without triggering a global dependency resolution
-uv pip install "bitsandbytes>=0.49.1" # to be in sync with transformers
-
 python src/quantize.py
--- src/quantize.py
+++ src/quantize.py
@@ -82,8 +82,7 @@ def process_model(model_id: str, s3_client):
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.bfloat16,
-        bnb_4bit_use_double_quant=True,
-        llm_int8_enable_fp32_cpu_offload=True  # Allow offloading at edge of OOM
+        bnb_4bit_use_double_quant=True
     )
     
     # 2. Download and load into VRAM
